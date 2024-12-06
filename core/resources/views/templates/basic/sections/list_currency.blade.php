1ce2e3a6676fdd5b8c0b7f0f864df07bc25ef1fb
Thoai Xuan <thoailata1000@gmail.com>
2024-12-06 15:03:54 +0700

    fix currecy_cow table

--- core/resources/views/templates/basic/sections/list_currency.blade.php
+++ core/resources/views/templates/basic/sections/list_currency.blade.php
@@ -1,3 +1,6 @@
+@php
+    $content = getContent('currency_cow.content', true);
+@endphp
 <div class = "py-120 table-section ">
     <div class="table-section__shape light-mood">
         <img src="{{ asset($activeTemplateTrue.'images/shapes/table-1.png') }}">
@@ -6,10 +9,9 @@
         <img src="{{ asset($activeTemplateTrue.'images/shapes/table-12.png') }}">
     </div>
     <div class="container">
+        <h2>{{ __(@$content->data_values->table_heading) }}</h2>
         <div class="row">
-            
             <x-flexible-view :view="$activeTemplate.'sections.currency_cow'" :meta="['from_section' => true ]" />
-        
         </div>
     </div>
 </div>
