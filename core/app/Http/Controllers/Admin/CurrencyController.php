f601cca1dd114ffecfc4914c82390463c877f2bf
Thoai Xuan <thoailata1000@gmail.com>
2024-12-09 12:30:14 +0700

    fix currecy_cow table

--- core/app/Http/Controllers/Admin/CurrencyController.php
+++ core/app/Http/Controllers/Admin/CurrencyController.php
@@ -114,6 +114,7 @@ class CurrencyController extends Controller
         $currency->sign             = $request->sign ?? null;
         $currency->p2p_sn           = $request->p2p_sn ?? 0;
         $currency->basicunit        = $request->basicunit ?? 0;
+        $currency->minorSingle      = $request->minorSingle ?? null;
         $currency->highlighted_coin = $request->is_highlighted_coin ? Status::YES : Status::NO;
         $currency->iscow            = $request->is_cow ? Status::YES : Status::NO;
 
