8bd8c343a6d5721e69d2fb72aee2b6e9160f9a6a
Thoai Xuan <thoailata1000@gmail.com>
2024-12-21 16:58:29 +0700

    update

--- core/resources/views/templates/basic/trending/trending.blade.php
+++
@@ -1,8 +0,0 @@
-@extends($activeTemplate.'layouts.frontend')
-@section('content')
-<div class="market-overview py-50 section-bg">
-    <div class="container">
-        <h1>@lang('Trending')</h1>
-    </div>
-</div>
-
