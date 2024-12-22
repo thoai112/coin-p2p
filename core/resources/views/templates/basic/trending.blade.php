641026ffd8402abeb0809e0054829248f31bccad
Thoai Xuan <thoailata1000@gmail.com>
2024-12-22 16:05:05 +0700

    update trading

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -222,8 +222,8 @@
             let lastPrice = 0;
             let investmentPriceLine = null;
             let webSocket = null;
-            let chartWidth = 1500px;
-            let chartHeight = 200px;
+            let chartWidth = Math.ceil($(".trade-section__left").width());
+            let chartHeight = Math.ceil($(".trade-section__left").height());
             let chartProperties = null;
             let direction;
             let dataIds = [];
