4faadef729f7895e26ad4b4aa4f9b01f69e9f209
Thoai Xuan <thoailata1000@gmail.com>
2024-12-23 15:09:21 +0700

    update trading

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -118,7 +118,6 @@
 
         #chart-container {
             width: 100%;
-            height: 400px;
             background-color: transparent;
             position: relative;
             overflow: hidden;
