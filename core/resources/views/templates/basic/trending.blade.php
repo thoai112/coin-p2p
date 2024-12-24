48d4505fb1550232d6a79536c2121addd55fae6e
Thoai Xuan <thoailata1000@gmail.com>
2024-12-24 18:10:09 +0700

    update

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -49,8 +49,8 @@
                                         </div>
                                         <div class="asset-compact-card__content">
                                             @if ($currency->type == Status::TRENDINGTYPE_CRYPTO)
+                                                <span> {{ gettype($currency->rate) }}</span>
                                                 @php
-                                                   <span> {{ gettype($currency->rate) }}</span>
                                                     $data = [0, 2, 1, 3, 3, 2, 1, 5, 4];
                                                     $svg = LineChart::new($data)
                                                         ->withColorGradient(
