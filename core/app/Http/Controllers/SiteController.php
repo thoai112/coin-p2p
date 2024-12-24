45fab7b984a5febcd755852cdfae42c3ad1ffac2
Thoai Xuan <thoailata1000@gmail.com>
2024-12-24 15:57:30 +0700

    update trending

--- core/app/Http/Controllers/SiteController.php
+++ core/app/Http/Controllers/SiteController.php
@@ -74,18 +74,17 @@ class SiteController extends Controller
 
     public function getValueTrending($currencies)
     {   
-        $trending = [];
         foreach ($currencies as $currency) {
            if ($currency->type != Status::TRENDINGTYPE_CRYPTO) continue;
             
            $url = "https://api.binance.com/api/v3/klines?symbol=".strtoupper($currency->symbol) ."USDT&interval=1m&limit=2000";
             $response = CurlRequest::curlContent($url);
             $array = json_decode($response, true); 
-            $object = (object) $array;
-            $trending[] = $object;
+            $object = $array;
+            $currency->rate = $object;
            
         }
-        return(object) $trending;
+        return $currencies;
     }
 
     public function contactSubmit(Request $request)
