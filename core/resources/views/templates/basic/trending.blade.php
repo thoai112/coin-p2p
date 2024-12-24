e7e122cef56415ab9dc6aa6ab81b38f49fa716d4
Thoai Xuan <thoailata1000@gmail.com>
2024-12-25 00:16:13 +0700

    update

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -56,7 +56,7 @@
 
                                                     $datax = json_decode(json_encode($currency->rate), true);
                                                     // $specificValue = $datax[0];
-                                                    print_r($datax);
+                                                    print_r(gettype($datax));
                                                     // // Extract specific values
 
                                                     // $svg = LineChart::new($data)
