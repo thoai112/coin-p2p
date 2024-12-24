5b8f7699c1b047f356e53c248565d91d6230a23a
Thoai Xuan <thoailata1000@gmail.com>
2024-12-24 15:31:41 +0700

    update trending

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -26,17 +26,8 @@
                 </div>
                 <div class="trade-section__right">
                     <span>{{ gettype($trendingx) }} </span>
-                    @foreach ($trendingx as $object)
-                        @foreach ($object as $array)
-                            <p>Timestamp: {{ $array[0] }}</p>
-                            <p>Open: {{ $array[1] }}</p>
-                            <p>High: {{ $array[2] }}</p>
-                            <p>Low: {{ $array[3] }}</p>
-                            <p>Close: {{ $array[4] }}</p>
-                            <p>Volume: {{ $array[5] }}</p>
-                            <!-- Add more fields as needed -->
-                        @endforeach
-                    @endforeach
+                    <pre>{{ print_r($trendingx, true) }}</pre>
+                    
                     <nav class="nav-horizontal">
                         {{-- <button class="nav-horizontal__btn prev"><i class="las la-angle-left"></i></button>
                         <button class="nav-horizontal__btn next"><i class="las la-angle-right"></i></button> --}}
