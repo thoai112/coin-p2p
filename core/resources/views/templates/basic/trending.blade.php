6f5c2f4b86df8d9e9160538ed9bf60dad75744b1
Thoai Xuan <thoailata1000@gmail.com>
2024-12-24 16:18:52 +0700

    update trending

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -45,8 +45,9 @@
                                             </h6>
                                         </div>
                                         <div class="asset-compact-card__content">
-                                            <h6 class="asset-compact-card__title">{{ @$langDetails->code }} </h6>
-                                            <h6 class="asset-compact-card__title">{{ $currency->name }}</h6>
+                                            <svg height="80" width="100">
+                                                <polyline points="{{ $currency->rate[0][0] }},0 {{ $currency->rate[0][4] }},100" style="fill:none;stroke:black;stroke-width:3" />
+                                            </svg>
                                         </div>
                                         <div class="asset-compact-card__content">
                                             <h6 class="asset-compact-card__title">{{ $currency->name }}</h6>
