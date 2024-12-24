4e4584e5c72555922307d168c5c5c4ff784b6301
Thoai Xuan <thoailata1000@gmail.com>
2024-12-24 18:44:33 +0700

    update

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -52,8 +52,8 @@
                                                 {{-- <span> {{ print_r($currency->rate) }}</span> --}}
                                                 @php
                                                     $data = [0, 2, 1, 3, 3, 2, 1, 5, 4];
-                                                   
-                                                    print_r(gettype($currency->rate[0]));
+                                                    
+                                                    print_r(gettype(json_decode($currency->rate, true);));
                                                     $svg = LineChart::new($data)
                                                         ->withColorGradient(
                                                             'rgb(48, 231, 237)',
