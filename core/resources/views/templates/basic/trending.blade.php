a7cba1db9aab4fb5f5610f1568d1cf0a3fb49612
Thoai Xuan <thoailata1000@gmail.com>
2024-12-24 14:57:01 +0700

    update trending

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -25,7 +25,7 @@
                     </div>
                 </div>
                 <div class="trade-section__right">
-                    <span>{{ $trendingx }} </span>
+                    <span>{{ gettype($trendingx) }} </span>
                     
                     <nav class="nav-horizontal">
                         {{-- <button class="nav-horizontal__btn prev"><i class="las la-angle-left"></i></button>
