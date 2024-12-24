6111dbc42e6b368a1f8384fd035a429766ee0abe
Thoai Xuan <thoailata1000@gmail.com>
2024-12-24 16:06:36 +0700

    update trending

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -50,7 +50,7 @@
                                         </div>
                                         <div class="asset-compact-card__content">
                                             <h6 class="asset-compact-card__title">{{ $currency->name }}</h6>
-                                            <h6 class="asset-compact-card__title">{{ parseFloat($currency->rate[0][4],4) ?? $currency->symbol}}</h6>
+                                            <h6 class="asset-compact-card__title">{{ number_format($currency->rate[0][4], 2) ?? $currency->symbol}}</h6>
                                         </div>
                                     </div>
                                 </li>
