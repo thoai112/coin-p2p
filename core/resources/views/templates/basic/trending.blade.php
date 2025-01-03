19dec75eafb2190aba0c89f3d3621ac9e58dcce4
Thoai Xuan <thoailata1000@gmail.com>
2025-01-03 16:35:04 +0700

    update

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -38,7 +38,7 @@
                     </div>
                 </div>
                 <div class="trade-section__right">
-                    <h6 class="asset-card__title">@lang('Trending') </h6>
+                    <h6 class="timer-value">@lang('Trending') </h6>
                     <nav class="nav-horizontal">
                         {{-- <button class="nav-horizontal__btn prev"><i class="las la-angle-left"></i></button>
                         <button class="nav-horizontal__btn next"><i class="las la-angle-right"></i></button> --}}
