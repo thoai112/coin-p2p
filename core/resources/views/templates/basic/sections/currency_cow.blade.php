9d8acdbd6b2ad2aa24c742a3c0170fcb1a72fb52
Thoai Xuan <thoailata1000@gmail.com>
2025-01-14 02:02:05 +0700

    update

--- core/resources/views/templates/basic/sections/currency_cow.blade.php
+++ core/resources/views/templates/basic/sections/currency_cow.blade.php
@@ -82,6 +82,24 @@
             </span>
             ) / 158
         </p>
+        <span className="cow-row">
+            <span className="average-price"></span>
+            <span className="cow">1 Cow</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 17"
+              aria-hidden="true"
+              className="custom-convert"
+            >
+              <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M11.726 1.273l2.387 2.394H.667V5h13.446l-2.386 2.393.94.94 4-4-4-4-.94.94zM.666 12.333l4 4 .94-.94L3.22 13h13.447v-1.333H3.22l2.386-2.394-.94-.94-4 4z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+        </span>
         {{-- @foreach ($elements as $element)
                 <p class="coincheck-item__desc"> {{ __(@$element->data_values->subheading) }}</p>
             @endforeach --}}
@@ -334,7 +352,7 @@
 
                         $('#cow-value').html(
                         `<span id="cow-value"> 1 COW = ${resp.cow.toFixed(5)} </span>`);
-
+                        $('.average-price').html(`<span className="average-price">${resp.cow.toFixed(3)} VND = 1 COW</span>`);
 
                         $('.load-more-market-list').removeClass('d-none');
                         loadMore ? $('#market-list-body').append(html) : $('#market-list-body').html(html);
