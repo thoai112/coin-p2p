3157bcce4b58e54a828799ad685935829bb47dda
Thoai Xuan <thoailata1000@gmail.com>
2025-01-13 16:30:17 +0700

    update

--- core/resources/views/templates/basic/sections/currency_cow.blade.php
+++ core/resources/views/templates/basic/sections/currency_cow.blade.php
@@ -87,40 +87,63 @@
         (function($) {
             "use strict"
 
+            // const datePicker = $('.date-range').daterangepicker({
+            //     autoUpdateInput: false,
+            //     locale: {
+            //         cancelLabel: 'Clear'
+            //     },
+            //     showDropdowns: true,
+            //     ranges: {
+            //         'Today': [moment(), moment()],
+            //         'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            //         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            //         'Last 15 Days': [moment().subtract(14, 'days'), moment()],
+            //         'Last 30 Days': [moment().subtract(30, 'days'), moment()],
+            //         'This Month': [moment().startOf('month'), moment().endOf('month')],
+            //         'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month')
+            //             .endOf('month')
+            //         ],
+            //         'Last 6 Months': [moment().subtract(6, 'months').startOf('month'), moment().endOf('month')],
+            //         'This Year': [moment().startOf('year'), moment().endOf('year')],
+            //     },
+            //     maxDate: moment()
+            // });
+
+
+            // const changeDatePickerText = (event, startDate) => {
+            //     $(event.target).val(startDate.format('MMMM DD, YYYY'));
+            // }
+
+            // $('.date-range').on('apply.datepicker', (event, picker) => changeDatePickerText(event, picker
+            //     .startDate));
+
+            // if ($('.date-range').val()) {
+            //     let dateRange = $('.date-range').val().split(' - ');
+            //     $('.date-range').data('datepicker').setStartDate(new Date(dateRange[0]));
+            //     // $('.date-range').data('daterangepicker').setEndDate(new Date(dateRange[1]));
+            // }
+
             const datePicker = $('.date-range').daterangepicker({
-                autoUpdateInput: false,
+                singleDatePicker: true, // Enable single date selection
+                autoUpdateInput: false, // Prevent automatic value update
+                showDropdowns: true, // Allow year/month dropdowns
                 locale: {
-                    cancelLabel: 'Clear'
+                    format: 'MMMM DD, YYYY', // Date format
+                    cancelLabel: 'Clear' // Label for the clear button
                 },
-                showDropdowns: true,
-                ranges: {
-                    'Today': [moment(), moment()],
-                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                    'Last 15 Days': [moment().subtract(14, 'days'), moment()],
-                    'Last 30 Days': [moment().subtract(30, 'days'), moment()],
-                    'This Month': [moment().startOf('month'), moment().endOf('month')],
-                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month')
-                        .endOf('month')
-                    ],
-                    'Last 6 Months': [moment().subtract(6, 'months').startOf('month'), moment().endOf('month')],
-                    'This Year': [moment().startOf('year'), moment().endOf('year')],
-                },
-                maxDate: moment()
+                maxDate: moment() // Set the maximum date to today
             });
 
-            const changeDatePickerText = (event, startDate) => {
-                $(event.target).val(startDate.format('MMMM DD, YYYY'));
-            }
+            // Update input field with the selected date
+            $('.date-range').on('apply.daterangepicker', function(event, picker) {
+                $(this).val(picker.startDate.format('MMMM DD, YYYY')); // Format date and update input
+            });
 
-            $('.date-range').on('apply.datepicker', (event, picker) => changeDatePickerText(event, picker
-                .startDate));
+            // Clear input field on cancel
+            $('.date-range').on('cancel.daterangepicker', function() {
+                $(this).val(''); // Clear the input field
+            });
 
-            if ($('.date-range').val()) {
-                let dateRange = $('.date-range').val().split(' - ');
-                $('.date-range').data('datepicker').setStartDate(new Date(dateRange[0]));
-                // $('.date-range').data('daterangepicker').setEndDate(new Date(dateRange[1]));
-            }
         })(jQuery);
     </script>
     <style>
