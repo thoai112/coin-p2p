df1c831dd11ae4c34028530563e8fdbb78c08775
Thoai Xuan <thoailata1000@gmail.com>
2025-01-14 03:07:38 +0700

    update

--- core/resources/views/templates/basic/sections/currency_cow.blade.php
+++ core/resources/views/templates/basic/sections/currency_cow.blade.php
@@ -88,7 +88,7 @@
             <i class="las la-retweet"></i>
             <span id="selectedCurrency">None</span>
             <select id="currency" name="currency">
-                <option value="">@lang('Select')</option>
+                <option value="">Select</option>
             </select>
         </span>
 
