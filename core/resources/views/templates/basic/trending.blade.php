15fb0a3713448f411ff9be3b99cc6fd358959639
Thoai Xuan <thoailata1000@gmail.com>
2025-01-14 15:43:03 +0700

    update

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -13,6 +13,36 @@
         <div class="tradingview-widget-container__widget"></div>
         <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow"
                 target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+            {
+                "symbols": [{
+                        "proName": "FOREXCOM:SPXUSD",
+                        "title": "S&P 500 Index"
+                    },
+                    {
+                        "proName": "FOREXCOM:NSXUSD",
+                        "title": "US 100 Cash CFD"
+                    },
+                    {
+                        "proName": "FX_IDC:EURUSD",
+                        "title": "EUR to USD"
+                    },
+                    {
+                        "proName": "BITSTAMP:BTCUSD",
+                        "title": "Bitcoin"
+                    },
+                    {
+                        "proName": "BITSTAMP:ETHUSD",
+                        "title": "Ethereum"
+                    }
+                ],
+                "showSymbolLogo": true,
+                "isTransparent": true,
+                "displayMode": "adaptive",
+                "colorTheme": "dark",
+                "locale": "en"
+            }
+        </script>
     </div>
     <!-- TradingView Widget END -->
     <section class="trade-section">
@@ -354,9 +384,9 @@
         @media (max-width: 768px) {
 
             /* .time-selection {
-                                    flex-direction: column;
-                                    align-items: flex-start;
-                                } */
+                                        flex-direction: column;
+                                        align-items: flex-start;
+                                    } */
 
             .time-selection label {
                 margin-bottom: 10px;
@@ -768,38 +798,4 @@
 
         })(jQuery)
     </script>
-@endpush
-
-@push('script')
-    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-        {
-            "symbols": [{
-                    "proName": "FOREXCOM:SPXUSD",
-                    "title": "S&P 500 Index"
-                },
-                {
-                    "proName": "FOREXCOM:NSXUSD",
-                    "title": "US 100 Cash CFD"
-                },
-                {
-                    "proName": "FX_IDC:EURUSD",
-                    "title": "EUR to USD"
-                },
-                {
-                    "proName": "BITSTAMP:BTCUSD",
-                    "title": "Bitcoin"
-                },
-                {
-                    "proName": "BITSTAMP:ETHUSD",
-                    "title": "Ethereum"
-                }
-            ],
-            "showSymbolLogo": true,
-            "isTransparent": true,
-            "displayMode": "adaptive",
-            "colorTheme": "dark",
-            "locale": "en"
-        }
-    </script>
-
 @endpush
