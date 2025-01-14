b290afe5fe5d1600dcd41f520891160a1fe084e9
Thoai Xuan <thoailata1000@gmail.com>
2025-01-14 19:36:18 +0700

    update

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -210,7 +210,7 @@
                 <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js" async>
                     {
                         "feedMode": "all_symbols",
-                        "isTransparent": True,
+                        "isTransparent": true,
                         "displayMode": "regular",
                         "width": "100%",
                         "height": "100%",
