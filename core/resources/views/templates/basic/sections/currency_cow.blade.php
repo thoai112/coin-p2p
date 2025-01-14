484505755ae68a8ac9a591d40a705032ee4fe104
Thoai Xuan <thoailata1000@gmail.com>
2025-01-14 14:09:10 +0700

    update

--- core/resources/views/templates/basic/sections/currency_cow.blade.php
+++ core/resources/views/templates/basic/sections/currency_cow.blade.php
@@ -239,9 +239,9 @@
                 if (!search) return; // Exit if the search value is empty
 
                 $('.load-more-market-list').removeClass('d-none');
-                    $('#market-list-body .text-center')
-                        .remove();
-                }
+                //     $('#market-list-body .text-center')
+                //         .remove();
+                // }
                 
                 // Clear any existing highlights first
                 $('#market-list-body tr').removeClass('highlight'); // Target the correct rows
