5aab4b0f07db02ddde466ae6d9261e47ca01513b
Thoai Xuan <thoailata1000@gmail.com>
2025-01-14 15:25:27 +0700

    update

--- core/resources/views/templates/basic/trending.blade.php
+++ core/resources/views/templates/basic/trending.blade.php
@@ -8,6 +8,13 @@
         $langDetails = $languages->where('code', config('app.locale'))->first();
     @endphp
     <section class="trade-section">
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+            <div class="tradingview-widget-container__widget"></div>
+            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow"
+                    target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+        </div>
+        <!-- TradingView Widget END -->
         <div class="container-fluid container-fluid--custom">
             <div class="trade-section__inner">
                 <div class="trade-section__left">
@@ -335,7 +342,7 @@
             }
         }
 
-        @media (max-width: 991px){
+        @media (max-width: 991px) {
             .time-period-option {
                 font-size: 12px;
                 padding: 3px 10px;
@@ -346,9 +353,9 @@
         @media (max-width: 768px) {
 
             /* .time-selection {
-                flex-direction: column;
-                align-items: flex-start;
-            } */
+                    flex-direction: column;
+                    align-items: flex-start;
+                } */
 
             .time-selection label {
                 margin-bottom: 10px;
@@ -746,5 +753,39 @@
 
 
         })(jQuery)
+    </script>
+@endpush
+
+@push('script')
+<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+    {
+    "symbols": [
+      {
+        "proName": "FOREXCOM:SPXUSD",
+        "title": "S&P 500 Index"
+      },
+      {
+        "proName": "FOREXCOM:NSXUSD",
+        "title": "US 100 Cash CFD"
+      },
+      {
+        "proName": "FX_IDC:EURUSD",
+        "title": "EUR to USD"
+      },
+      {
+        "proName": "BITSTAMP:BTCUSD",
+        "title": "Bitcoin"
+      },
+      {
+        "proName": "BITSTAMP:ETHUSD",
+        "title": "Ethereum"
+      }
+    ],
+    "showSymbolLogo": true,
+    "isTransparent": true,
+    "displayMode": "adaptive",
+    "colorTheme": "dark",
+    "locale": "en"
+  }
     </script>
 @endpush
