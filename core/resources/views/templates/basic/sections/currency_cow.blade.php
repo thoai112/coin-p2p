02fa9a695843c00ae7faa07e6b68bb695e18b770
Thoai Xuan <thoailata1000@gmail.com>
2025-01-14 14:08:47 +0700

    update

--- core/resources/views/templates/basic/sections/currency_cow.blade.php
+++ core/resources/views/templates/basic/sections/currency_cow.blade.php
@@ -239,7 +239,7 @@
                 if (!search) return; // Exit if the search value is empty
 
                 $('.load-more-market-list').removeClass('d-none');
-                    $('#market-list-body text-center')
+                    $('#market-list-body .text-center')
                         .remove();
                 }
                 
